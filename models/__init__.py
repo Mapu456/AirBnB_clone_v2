f71883c8b319e3e8e35fe6cffd4e2beab2f30561
Mapu456 <mpaula456@hotmail.com>
2021-12-20 15:28:06 -0500

    pep8 __init__

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""This module instantiates an object of class FileStorage or DBStorage"""
 from os import getenv
 
 storage = None
