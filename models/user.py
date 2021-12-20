218677b32190333a118b8e43dac443f68eaa79b6
Mapu456 <mpaula456@hotmail.com>
2021-12-20 12:08:49 -0500

    user, satate, and city tables modified

--- models/user.py
+++ models/user.py
@@ -6,7 +6,6 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
@@ -14,5 +13,4 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
-
-places = relationship('Place', backref='user')
+    places = relationship('Place', backref='user')
