e8ca60eeb2c37acff44221c2867826eae7eb6217
Mapu456 <mpaula456@hotmail.com>
2021-12-20 13:09:35 -0500

    method all in db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,6 +4,11 @@
 from models.base_model import BaseModel, Base
 from os import getenv
 from sqlalchemy import (create_engine)
+from models.amenity import Amenity
+from models.city import City
+from models.state import State
+from models.place import Place
+from models.review import Review
 from models.user import User
 
 
@@ -22,3 +27,28 @@ class DBStorage():
             pool_pre_ping=True)
         if env == "test":
             Base.metadata.drop_all(bind=self.__engine)
+
+    def all(self, cls=None):
+        new_dictionary = {}
+        query = None
+        if cls is None:
+            tables_list = [User, State, City, Amenity, Place, Review]
+            for table in tables_list:
+                query += self.__session.query(table).all()
+            for new_object in query:
+                new_dictionary[new_object.to_dict()['__class__'] + '.' +
+                                    new_object.id] = new_object
+        else:
+            query = self.__session.query(cls).all()
+            for new_object in query:
+                new_dictionary[new_object.to_dict()['__class__'] + '.' +
+                                    new_object.id] = new_object
+
+    def new(self, obj):
+        pass
+    def save(self):
+        pass
+    def delete(self, obj=None):
+        pass
+    def reload(self):
+        pass
