732f380773065c349b210dae6cb01844471e7204
Mapu456 <mpaula456@hotmail.com>
2021-12-21 15:01:45 -0500

    tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" unitest for testing BaseModel class """
+""" """
 from models.base_model import BaseModel
 import unittest
 import datetime
@@ -9,39 +9,38 @@ import os
 
 
 class test_basemodel(unittest.TestCase):
-    """ Unittests for testing the basemodel class"""
+    """ """
 
     def __init__(self, *args, **kwargs):
-        """ Unittests for testing the basemodel class"""
+        """ """
         super().__init__(*args, **kwargs)
         self.name = 'BaseModel'
         self.value = BaseModel
 
     def setUp(self):
-        """ Unittests for testing the basemodel class"""
+        """ """
         pass
 
     def tearDown(self):
-        """ Unittests for testing the basemodel class"""
         try:
             os.remove('file.json')
         except:
             pass
 
     def test_default(self):
-        """ Unittests for testing the basemodel class"""
+        """ """
         i = self.value()
         self.assertEqual(type(i), self.value)
 
     def test_kwargs(self):
-        """ Unittests for testing the basemodel class"""
+        """ """
         i = self.value()
         copy = i.to_dict()
         new = BaseModel(**copy)
         self.assertFalse(new is i)
 
     def test_kwargs_int(self):
-        """ Unittests for testing the basemodel class"""
+        """ """
         i = self.value()
         copy = i.to_dict()
         copy.update({1: 2})
@@ -49,7 +48,7 @@ class test_basemodel(unittest.TestCase):
             new = BaseModel(**copy)
 
     def test_save(self):
-        """ Unittests for testing the basemodel class"""
+        """ Testing save """
         i = self.value()
         i.save()
         key = self.name + "." + i.id
@@ -58,41 +57,41 @@ class test_basemodel(unittest.TestCase):
             self.assertEqual(j[key], i.to_dict())
 
     def test_str(self):
-        """ Unittests for testing the basemodel class"""
+        """ """
         i = self.value()
         self.assertEqual(str(i), '[{}] ({}) {}'.format(self.name, i.id,
                          i.__dict__))
 
     def test_todict(self):
-        """ Unittests for testing the basemodel class"""
+        """ """
         i = self.value()
         n = i.to_dict()
         self.assertEqual(i.to_dict(), n)
 
     def test_kwargs_none(self):
-        """ Unittests for testing the basemodel class"""
+        """ """
         n = {None: None}
         with self.assertRaises(TypeError):
             new = self.value(**n)
 
     def test_kwargs_one(self):
-        """ Unittests for testing the basemodel class"""
+        """ """
         n = {'Name': 'test'}
         with self.assertRaises(KeyError):
             new = self.value(**n)
 
     def test_id(self):
-        """ Unittests for testing the basemodel class"""
+        """ """
         new = self.value()
         self.assertEqual(type(new.id), str)
 
     def test_created_at(self):
-        """ Unittests for testing the basemodel class"""
+        """ """
         new = self.value()
         self.assertEqual(type(new.created_at), datetime.datetime)
 
     def test_updated_at(self):
-        """ Unittests for testing the basemodel class"""
+        """ """
         new = self.value()
         self.assertEqual(type(new.updated_at), datetime.datetime)
         n = new.to_dict()
