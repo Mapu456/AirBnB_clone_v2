3075bd3a47fa3be27371e44f37bf25d8cddd821f
Mapu456 <mpaula456@hotmail.com>
2022-01-14 18:44:42 +0000

    point 0

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -4,7 +4,9 @@ the web_static folder function do_pack."""
 
 from fabric.api import *
 from datetime import datetime
-import os
+
+env.hosts = ['35.243.245.64', '18.234.66.163']
+env.user = "ubuntu"
 
 
 def do_pack():
@@ -21,34 +23,27 @@ def do_pack():
         return None
 
 
-env.hosts = ['35.243.245.64', '18.234.66.163']
-env.user = 'ubuntu'
-
-
 def do_deploy(archive_path):
-    """Fabric script that distributes an archive to your web servers,
-    using the function do_deploy"""
-    if os.path.isfile(archive_path) is False:
-        return False
-    else:
-        file_name = archive_path[9:]
-        name_dir = "/data/web_static/releases/" + file_name[:-4]
-        path_to_store = "/tmp/" + file_name
+    """Deploy the boxing package tgz file
+    """
+    try:
+        archive = archive_path.split('/')[-1]
+        path = '/data/web_static/releases/' + archive.strip('.tgz')
+        current = '/data/web_static/current'
+        put(archive_path, '/tmp/')
 
-        put(archive_path, "/tmp/")
-
-        run("sudo mkdir -p {}".format(name_dir))
-        run("sudo tar -xzf {} -C {}".format(path_to_store, name_dir))
-        run("sudo rm {}".format(path_to_store))
-        run("sudo mv {}/web_static/* {}".format(name_dir,
-                                                name_dir))
-        run("sudo rm -rf {}/web_static".format(name_dir))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current"
-            .format(name_dir))
-        print("New version deployed!")
-
+        run('mkdir -p {}/'.format(path))
+        run('tar -xzf /tmp/{} -C {}'.format(archive, path))
+        run('rm /tmp/{}'.format(archive))
+        run('mv {}/web_static/* {}'.format(path, path))
+        run('rm -rf {}/web_static'.format(path))
+        run('rm -rf {}'.format(current))
+        run('ln -s {} {}'.format(path, current))
+        print('New version deployed!')
         return True
+    except Exception as e:
+        print(e)
+        return False
 
 
 def deploy():
