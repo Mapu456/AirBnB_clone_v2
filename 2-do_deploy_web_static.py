15335ee4f0c853381a26bf2037b29c835a814977
Mapu456 <mpaula456@hotmail.com>
2022-01-14 17:52:25 +0000

    point 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -22,26 +22,30 @@ def do_pack():
 
 
 env.hosts = ['35.243.245.64', '18.234.66.163']
+env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
-    """Fabric script that generates a .tgz archive from the contents of
-    the web_static folder function do_pack"""
-    name_dir = archive_path[9:-4]
-    if os.path.exists(archive_path):
-        put(archive_path, '/tmp')
-        run("sudo mkdir -p /data/web_static/releases/{}".format(name_dir))
-        run("sudo tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/".
-            format(name_dir, name_dir))
-        run("sudo rm /tmp/{}.tgz".format(name_dir))
-        run("sudo mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{} /".format(name_dir, name_dir))
-        run("sudo rm -rf /data/web_static/releases/{}/web_static".
-            format(name_dir))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s /data/web_static/releases/{}/ \
-            /data/web_static/current".format(name_dir))
-        print("New version deployed!")
-        return True
-    else:
+    """Fabric script that distributes an archive to your web servers,
+    using the function do_deploy"""
+    if os.path.isfile(archive_path) is False:
         return False
+    else:
+        file_name = archive_path[9:]
+        name_dir = "/data/web_static/releases/" + file_name[:-4]
+        path_to_store = "/tmp/" + file_name
+
+        put(archive_path, "/tmp/")
+
+        run("sudo mkdir -p {}".format(name_dir))
+        run("sudo tar -xzf {} -C {}".format(path_to_store, name_dir))
+        run("sudo rm {}".format(path_to_store))
+        run("sudo mv {}/web_static/* {}".format(name_dir,
+                                                name_dir))
+        run("sudo rm -rf {}/web_static".format(name_dir))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current"
+            .format(name_dir))
+        print("New version deployed!")
+
+        return True
