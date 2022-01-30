c08b0b77bf286fceb128f6647e013d18cd793b93
Mapu456 <mpaula456@hotmail.com>
2022-01-30 11:39:21 -0500

    update file storage and db storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -67,3 +67,7 @@ class FileStorage:
             self.save()
         else:
             pass
+
+    def close(self):
+        """funcion for deserializing the JSON file to objects"""
+        self.reload()
